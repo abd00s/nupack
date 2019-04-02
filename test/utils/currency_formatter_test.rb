2760482d1f122e0d434a72af13dff1868e244fb2
Abdullah Pharaon <abdullah.pharaon@gmail.com>
2019-04-02 14:46:43 -0400

    spec #format_currency and #rounded_total_string

--- test/utils/currency_formatter_test.rb
+++ test/utils/currency_formatter_test.rb
@@ -36,6 +36,29 @@ class Utils::CurrencyFormatterTest < Minitest::Test
       end
     end
 
+    describe "#rounded_total_string" do
+      it "converts the numerical value to a string" do
+        assert new_instance(1111.11).send(:rounded_total_string).class == String
+      end
+
+      it "appends two decimal point zeros to an integer" do
+        assert_equal new_instance(1).send(:rounded_total_string),
+                     "1.00"
+      end
+
+      it "rounds value to 2nd decimal point" do
+        assert_equal new_instance(1.136).send(:rounded_total_string),
+                     "1.14"
+      end
+    end
+
+    describe "#format_currency" do
+      it "prepends a dollar symbol to the price amount" do
+        assert_equal new_instance(111.11).send(:format_currency),
+                     "$111.11"
+      end
+    end
+
     def new_instance(price)
       Utils::CurrencyFormatter.new(price: price)
     end
