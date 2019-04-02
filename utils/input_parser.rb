9b54905fa1b7a194efbdae8ca6486f861a506970
Abdullah Pharaon <abdullah.pharaon@gmail.com>
2019-04-02 13:43:01 -0400

    further abstarction in inout parser

--- utils/input_parser.rb
+++ utils/input_parser.rb
@@ -11,25 +11,36 @@ module Utils
     end
 
     def parse
-      @price            = strip_non_numerical_characters(price_input).to_f
-      @number_of_people = strip_non_numerical_characters(
-        number_of_people_input
-      ).to_i
-      @category         = split_input[2]
+      @price            = parsed_price
+      @number_of_people = parsed_number_of_people
+      @category         = parsed_category
     end
 
     private
+
+    def parsed_price
+      @parsed_price ||= strip_non_numerical_characters(price_input).to_f
+    end
 
     def price_input
       @price_input ||= split_input[0]
     end
 
-    def split_input
-      @split_input ||= @input_string.split(", ")
+    def parsed_number_of_people
+      @parsed_number_of_people ||=
+        strip_non_numerical_characters(number_of_people_input).to_i
     end
 
     def number_of_people_input
       @number_of_people_input ||= split_input[1]
+    end
+
+    def parsed_category
+      @parsed_category ||= split_input[2]
+    end
+
+    def split_input
+      @split_input ||= @input_string.split(", ")
     end
 
     def strip_non_numerical_characters(string)
